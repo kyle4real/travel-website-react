360f1de8b9d5c8e0188ec304b3d95b3adc8570a0
Kyle Friel <businessfriel@gmail.com>
2021-07-31 21:58:34 -0400

    useWindowResize hook.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import classes from "./Header.module.scss";
 
 import { CgMenuRight, CgClose } from "react-icons/cg";
 import LeafSVG from "../../assets/LeafSVG.js";
 import Nav from "./Nav/Nav";
+import useWindowSize from "../../hooks/useWindowSize";
 
 const Header = () => {
     const [menuOpen, setMenuOpen] = useState(false);
+
+    const size = useWindowSize();
+
+    useEffect(() => {
+        if (size.width > 768 && menuOpen) {
+            setMenuOpen(false);
+        }
+    }, [size, menuOpen]);
 
     const handleMenuToggle = () => {
         setMenuOpen((p) => !p);
